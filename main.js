3d446ee6d836b97db35d12f376dd5e9cb817682d
MrRio21 <108085558+MrRio21@users.noreply.github.com>
2022-12-01 00:10:32 +0200

    zx
    zx

---
+++ main.js
@@ -0,0 +1,35 @@
+// var wether= prompt("enter your Number");
+// if (wether < 30) {
+//     console.log("Cold");
+// }else{
+//     console.log("Hot");
+// }
+/////////////////////////////
+
+// var user= prompt("Enter Your Name");
+// while (!isNaN(user)) {
+//     var user = prompt("Enter Your Name");
+// }
+// console.log("Name :"+ user);
+
+// var birthDay= prompt("Enter Your Birth Day");
+// let count = 0;
+// while (isNaN(birthDay)) {
+//     var birthDay = prompt("Enter Your Birth Day");
+//     count++;
+//     if (count == 2) {
+//         birthDay = true
+//     }
+    
+// }
+// console.log(birthDay);
+/////////////////////////////////////////
+//////////////lab4//////////////////////
+let num1 = 12;
+    let num2=13;
+    let number1= String(num1)[1];
+    let number2= String(num2)[1];
+   
+
+   
+    console.log(number1+ number2);
